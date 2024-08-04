5c6c6a78269b5ce12baf68e828a85696059bfc00
Francisco Javier Ortiz Bonilla <javi@Franciscos-MacBook-Air.local>
2024-08-04 14:39:05 +0200

    Changed color, added about this webpage section

--- assets/js/index-audio-script.js
+++ assets/js/index-audio-script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const spanViolin = document.getElementById('ipa-javi');
+    const audibleElement = document.getElementById('ipa-javi');
     const audio = document.getElementById('audio-javi');
 
-    spanViolin.addEventListener('mouseover', () => {
+    audibleElement.addEventListener('mouseover', () => {
         audio.play();
     });
 
-    spanViolin.addEventListener('mouseout', () => {
+    audibleElement.addEventListener('mouseout', () => {
         audio.pause();
         audio.currentTime = 0; // Reset audio to the beginning
     });
